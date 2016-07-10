502a1e1d3f20e238ff26dfbc390109cf6908d78d
whh20092966 <395055557@qq.com>
2016-07-10 23:57:06 +0800

    add  project

---
+++ Order/Order/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  StarleaksDemoIOs
+//
+//  Created by Dambert on 3/10/16.
+//  Copyright © 2016 com.starleaks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ViewController : UIViewController
+
+
+
+@end
+
---
+++ Order/README.md
@@ -0,0 +1,3 @@
+# SingleDemoXCodeIOS
+
+Aplicacition that have a textbox and return tha number of characters from the input
